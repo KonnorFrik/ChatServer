346efdad71ef4ec6f21304d09ce203aa5b682206
Konnor <konnor.frik666@gmail.com>
2025-06-08 00:47:33 +0500

    main:/cmd/user_auth/v1: implement Create handler; write WrapError for handlers;

--- cmd/user_auth/v1/main.go
+++ cmd/user_auth/v1/main.go
@@ -6,12 +6,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"net"
 
+	"github.com/KonnorFrik/ChatServer/cmd/user_auth/v1/usecase"
 	recoverInterceptor "github.com/KonnorFrik/ChatServer/pkg/interceptor/recover"
 	logging "github.com/KonnorFrik/ChatServer/pkg/logging"
 	userAuthPb "github.com/KonnorFrik/ChatServer/pkg/user_auth/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
@@ -51,6 +55,35 @@ func main() {
         return
     }
 }
+
+// WrapError - wrap usecase error into gRPC error with codes
+func WrapError(err error) error {
+    var code = codes.Internal
+    var msg string
+    switch {
+    case errors.Is(err, usecase.ErrDoesNotExist):
+        code = codes.NotFound
+    case errors.Is(err, usecase.ErrAlreadyExist):
+        code = codes.AlreadyExists
+    case errors.Is(err, usecase.ErrInvalidData):
+        code = codes.InvalidArgument
+    case errors.Is(err, usecase.ErrDbNoAccess):
+        // default = Internal
+    case errors.Is(err, usecase.ErrUnknown):
+        // default = Internal
+    }
+
+    return status.Error(code, msg)
+}
+
 func (s *server) Create(ctx context.Context, req *userAuthPb.CreateUserRequest) (*userAuthPb.CreateUserResponse, error) {
-    panic("TEST PANIC")
+    var response userAuthPb.CreateUserResponse
+    user, err := usecase.Create(ctx, req)
+
+    if err != nil {
+        return nil, WrapError(err)
+    }
+
+    user.ToGrpcCreateResponse(&response)
+    return &response, status.Error(codes.OK, "")
 }
