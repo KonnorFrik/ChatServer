21f0a4e8cec6822657c54cd7d352209cdcf74c62
Konnor <konnor.frik666@gmail.com>
2025-06-09 18:39:05 +0500

    main:/db/migrations: add missed fields;

--- db/migrations/20250607144138_create_user_table.up.sql
+++ db/migrations/20250607144138_create_user_table.up.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password TEXT NOT NULL,
-    role INTEGER CHECK ( role <> 0 )
+    role INTEGER CHECK ( role <> 0 ),
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL
 );
